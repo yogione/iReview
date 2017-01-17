de384cf9355a46d529c0e22f48dfdddc57b08bee
Srini Motheram <srini@srinimoeramsmbp.wowway.com>
2017-01-16 23:12:06 -0500

    task complete

--- iReview/ViewController.swift
+++ iReview/ViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var restaurantNameLabel           :UILabel!
+    @IBOutlet var yourNameTextField             :UITextField!
+    @IBOutlet var yourReviewTextView            :UITextView!
+    @IBOutlet var overallRatingSlider           :UISlider!
+    @IBOutlet var foodRatingSegControl          :UISegmentedControl!
+    @IBOutlet var serviceRatingStepper          :UIStepper!
+    @IBOutlet var recommendedSwitch             :UISwitch!
+    
+    //MARK: - Interactivity Methods
+    @IBAction func pressed(button: UIButton){
+        guard let name = yourNameTextField.text else {
+            return
+        }
+        guard let review = yourReviewTextView.text else {
+            return
+        }
+        let foodRating = foodRatingSegControl.titleForSegment(at: foodRatingSegControl.selectedSegmentIndex)!
+        
+        
+        print("Review for Michigan Masala Restuarant")
+        print("Name: \(name)")
+        print("Review: \(review)")
+        print("Overall Rating: \(overallRatingSlider.value)")
+        print("Food Rating: \(foodRating)")
+        print("Service Rating: \(serviceRatingStepper.value)")
+        print("Recommended: \(recommendedSwitch.isOn)")
+
+       // surveyNameLabel.text = "\(name) Favorite Restaurant"
+ 
+        
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //userFirstNameTextField.delegate = self
+        recommendedSwitch.isOn = false
+        serviceRatingStepper.value = 4
+        overallRatingSlider.value = 0.5
+        foodRatingSegControl.selectedSegmentIndex = 1
     }
 
     override func didReceiveMemoryWarning() {
